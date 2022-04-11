a380af08c3b96710e5cd118d79bb2da23c3b3da8
rmaejima <ryo.maejii@gmail.com>
2022-04-11 17:36:07 +0900

    Add download schema shell

--- download-schema.sh
+++ download-schema.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
 # https://stackoverflow.com/questions/43267413/shell-how-to-set-environment-variables-from-env-file
-set -a # 環境変数の読み出しってこれであってますかね?
-source .env.local
+set -a 
+source .env
 set +a
 
 # memo: add $1 to specify dir
-yarn run apollo schema:download --header "Authorization: Bearer $CONTENTFUL_ACCESS_TOKEN" \
---endpoint https://graphql.contentful.com/content/v1/spaces/$CONTENTFUL_SPACE_ID/environments/master $1
+npx apollo schema:download --header "Authorization: Bearer $REACT_APP_CONTENTFUL_ACCESS_TOKEN" \
+--endpoint https://graphql.contentful.com/content/v1/spaces/$REACT_APP_CONTENTFUL_SPACE_ID/environments/master $1
